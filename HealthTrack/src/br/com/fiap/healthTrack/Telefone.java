9f09d9ebdc9f2d73596a4f1d33cf17f5c24f6c40
Daniel Caires <danielociares@gmail.com>
2020-09-11 11:48:09 -0300

    Comentários, constructors, getters e setters da classe Telefone

--- HealthTrack/src/br/com/fiap/healthTrack/Telefone.java
+++ HealthTrack/src/br/com/fiap/healthTrack/Telefone.java
@@ -1,17 +1,104 @@
 package br.com.fiap.healthTrack;
 
+/**
+ * Classe Telefone
+ * @author Leandro Dias
+ * @author Daniel Caires
+ * @version 1.0
+ */
 public class Telefone {
+	/** 
+	 * Ddi 
+	 */
 	int ddi;
+
+	/** 
+	 * Ddd 
+	 */
 	int ddd;
-	int prefixo;
-	int sufixo;
+	
+	/** 
+	 * Tipo de Contato 
+	 */
 	String tipoContato;
 	
+	/** 
+	 * Prefixo 
+	 */
+	int prefixo;
+	
+	/** 
+	 * Sufixo 
+	 */
+	int sufixo;
+	
+	public Telefone() {
+		
+	}
+
+	public Telefone(int ddi, int ddd, String tipoContato, int prefixo, int sufixo) {
+		this.setDdi(ddi)
+		this.setTipoContato(tipoContato)
+		this.setPrefixo(prefixo)
+		this.setSufixo(sufixo)
+		this.setDdd(ddd)
+	}
+	
+	/**
+	 * Adiciona um novo telefone
+	 */
 	public void incluir() {
 		// Lógica
 	}
 	
+	/**
+	 * Adiciona um telefone
+	 */
 	public void atualizar() {
 		// Lógica
+	}
+
+	public int getDdi() {
+		return ddi;
+	}
+	
+	public void setDdi(int ddi) {
+		this.ddi = ddi;
+	}
+	
+
+	public String getTipoContato() {
+		return tipoContato;
+	}
+	
+	public void setTipoContato(String tipoContato) {
+		this.tipoContato = tipoContato;
+	}
+	
+
+	public int getPrefixo() {
+		return prefixo;
+	}
+	
+	public void setPrefixo(int prefixo) {
+		this.prefixo = prefixo;
+	}
+	
+
+	public int getSufixo() {
+		return sufixo;
+	}
+	
+	public void setSufixo(int sufixo) {
+		this.sufixo = sufixo;
+	}
+	
+
+	public int getDdd() {
+		return ddd;
+	}
+	
+	public void setDdd(int ddd) {
+		this.ddd = ddd;
 	}
 }
